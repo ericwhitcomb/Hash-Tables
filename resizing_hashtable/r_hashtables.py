dec69fa5d282b6a5a92a51bbda5310ecd72fa786
Eric Whitcomb <eric.whitcomb@gmail.com>
2019-08-05 09:48:52 -0600

    Insert, remove, retrieve is complete in r_hashtables.py

--- resizing_hashtable/r_hashtables.py
+++ resizing_hashtable/r_hashtables.py
@@ -17,13 +17,18 @@ class LinkedPair:
 # '''
 class HashTable:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = [None] * self.capacity
 
 
 # '''
 # Research and implement the djb2 hash function
 # '''
-def hash(string, max):
+def hash(string, max):                                                                                                                     
+    hash = 5381
+    for c in string:
+        hash = (( hash << 5) + hash) + ord(c)
+    return hash % max
     pass
 
 
@@ -33,7 +38,18 @@ def hash(string, max):
 # Hint: Used the LL to handle collisions
 # '''
 def hash_table_insert(hash_table, key, value):
-    pass
+    index = hash(key, hash_table.capacity)
+    current_pair = hash_table.storage[index]
+
+    while current_pair is not None and current_pair.key != key:
+        current_pair = current_pair.next
+    
+    if current_pair is None:
+        pair = LinkedPair(key, value)
+        pair.next = hash_table.storage[index]
+        hash_table.storage[index] = pair
+    else:
+        current_pair.value = value
 
 
 # '''
@@ -42,7 +58,18 @@ def hash_table_insert(hash_table, key, value):
 # If you try to remove a value that isn't there, print a warning.
 # '''
 def hash_table_remove(hash_table, key):
-    pass
+    index = hash(key, hash_table.capacity)
+    current_pair = hash_table.storage[index]
+    last_pair = None
+
+    while current_pair is not None and current_pair.key != key:
+        last_pair = current_pair
+        current_pair = current_pair.next
+    
+    if current_pair is None:
+        print ("Warning: key " + str(key) + " not found")
+    else:
+        last_pair.next = current_pair.next
 
 
 # '''
@@ -51,7 +78,17 @@ def hash_table_remove(hash_table, key):
 # Should return None if the key is not found.
 # '''
 def hash_table_retrieve(hash_table, key):
-    pass
+    # get the index in the hash table
+    index = hash(key, hash_table.capacity)
+    current_pair = hash_table.storage[index]
+
+    while current_pair is not None and current_pair.key != key:
+        current_pair = current_pair.next
+    
+    if current_pair is None:
+        return None
+    else:
+        return current_pair.value
 
 
 # '''
@@ -62,22 +99,31 @@ def hash_table_resize(hash_table):
 
 
 def Testing():
-    ht = HashTable(2)
+    ht = HashTable(16)
+    hash_table_insert(ht, "A", "Value for A")
+    hash_table_insert(ht, "Q", "Value for Q")
+    print(hash_table_retrieve(ht, "A"))
+    print(hash_table_retrieve(ht, "Q"))
+    hash_table_remove(ht, "A")
+    print(hash_table_retrieve(ht, "A"))
+    print(hash_table_retrieve(ht, "Q"))
 
-    hash_table_insert(ht, "line_1", "Tiny hash table")
-    hash_table_insert(ht, "line_2", "Filled beyond capacity")
-    hash_table_insert(ht, "line_3", "Linked list saves the day!")
+    # ht = HashTable(2)
 
-    print(hash_table_retrieve(ht, "line_1"))
-    print(hash_table_retrieve(ht, "line_2"))
-    print(hash_table_retrieve(ht, "line_3"))
+    # hash_table_insert(ht, "line_1", "Tiny hash table")
+    # hash_table_insert(ht, "line_2", "Filled beyond capacity")
+    # hash_table_insert(ht, "line_3", "Linked list saves the day!")
 
-    old_capacity = len(ht.storage)
-    ht = hash_table_resize(ht)
-    new_capacity = len(ht.storage)
+    # print(hash_table_retrieve(ht, "line_1"))
+    # print(hash_table_retrieve(ht, "line_2"))
+    # print(hash_table_retrieve(ht, "line_3"))
 
-    print("Resized hash table from " + str(old_capacity)
-          + " to " + str(new_capacity) + ".")
+    # old_capacity = len(ht.storage)
+    # ht = hash_table_resize(ht)
+    # new_capacity = len(ht.storage)
+
+    # print("Resized hash table from " + str(old_capacity)
+    #       + " to " + str(new_capacity) + ".")
 
 
 Testing()
